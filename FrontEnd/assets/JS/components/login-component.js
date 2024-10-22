d5c597beffc05b1dd9c2f419c4c6058e58fcde11
iArtOC <itrd.1rt+github@gmail.com>
2024-10-22 12:58:44 +0200

    feat: created modal

--- FrontEnd/assets/JS/components/login-component.js
+++ FrontEnd/assets/JS/components/login-component.js
@@ -35,6 +35,7 @@ form.addEventListener("submit", async function (event) {
     }
 });
 
+// Récupérer le button login et le modifier si utilisateur connecté
 var buttonLogin = document.getElementById("btnLogin");
 if (userIsConnected() === true) {
     // Changer le texte en "logout" si le token est présent
@@ -44,16 +45,86 @@ if (userIsConnected() === true) {
     console.log ("déconnecté")
 }
 
-
+// Ajout d'une action au button 'login' pour déconnecté
 buttonLogin.addEventListener('click', function() {
     disconnected ();
 });
 
 function disconnected() {
-    if (userIsConnected === null) {
+    if (userIsConnected === false) {
         window.location.replace('./login.html');
     } else {
         localStorage.removeItem('token');
         buttonLogin.textContent = 'login';
     }
+}
+
+// Affichage en-tête 'mode édition'
+if (userIsConnected () === true) {
+    var body = document.querySelector("body")
+    if (body) {
+        var bannerEdit = document.createElement("div")
+        if (bannerEdit) {
+            bannerEdit.id="banner-edit-mode"
+            var editMode = document.createElement("p")
+            if (editMode) {
+                editMode.innerHTML=`
+                <i class="fa-solid fa-pen-to-square"></i>
+                Mode édition
+                ` 
+                bannerEdit.append(editMode)
+                body.prepend(bannerEdit)
+            }
+        }
+    }
+    // Sélectionner l'élément h2
+    var h2 = document.querySelector('#portfolio h2');
+
+    // Créer un bouton
+    var editBtn = document.createElement('span');
+    editBtn.innerHTML=`
+    <a><i class="fa-solid fa-pen-to-square">Modifier</i></a>
+    `;
+
+    // Ajouter le bouton après l'élément h2
+    h2.insertAdjacentElement('beforeend', editBtn);
+
+    // Ajouter un gestionnaire d'événements (facultatif)
+    editBtn.addEventListener('click', function () {
+       modal ();
+       function modal (){
+        // Création de la modal
+        if (body) {
+        var globalModal = document.createElement ("div");
+            if (globalModal) {
+            globalModal.classList.add ("modal");
+
+                // Création du contenu de la modal
+                var firstModal = document.createElement ("div");
+                if (firstModal) {
+                    firstModal.classList.add ("content-modal");
+                    firstModal.innerHTML=`
+                    <div class="content-modal">
+                        <div class="exit-modal">
+                            <span><i class="fa-solid fa-xmark"></i></span>
+                        </div>
+
+                        <h2>Galerie photo</h2>
+
+                        <div id="tinyGalleryGrid" class="tinyGalleryGrid">
+                        </div>
+
+                        <div class="ajouter-supprimer">
+                            <button id="addWorks" type="submit">Ajouter une photo</button>
+                        </div>
+                    </div>
+                    `
+                    globalModal.append(firstModal);
+                }
+
+            body.append(globalModal);
+            }
+        } 
+       }
+    });
 }
